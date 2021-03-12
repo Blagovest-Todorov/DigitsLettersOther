ff069aa0ae347d2c0db0f9c0857f87e8750c51b6
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-03-12 19:06:41 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine(); //read teh String from Console
+
+            StringBuilder digits = new StringBuilder(line.Length);
+            StringBuilder letters = new StringBuilder(line.Length);
+            StringBuilder others = new StringBuilder(line.Length);
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (Char.IsDigit(line[i]))
+                {
+                    digits.Append(line[i]);
+                }
+                else if (Char.IsLetter(line[i]))
+                {
+                    letters.Append(line[i]);
+                }
+                else // if other than letter or digit
+                {
+                    others.Append(line[i]);
+                }               
+            }
+
+            Console.Write(digits);
+            Console.WriteLine();
+
+            Console.Write(letters);
+            Console.WriteLine();
+
+            Console.Write(others);
+            Console.WriteLine();
+
+            
+        }
+    }
+}
+
